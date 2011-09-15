5a6c68cb596e5c6afa19077330dd9c04ab4cc217
Paul Slugocki <paulslugocki@gmail.com>
2011-09-15 11:56:14 +0100

    Added images to Tour Search

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 include("inc/config.php");
 
 // Get basic endpoint that's being called
@@ -6,7 +7,7 @@ isset($_GET['url']) ? $url = $_GET['url'] : $url = $_GET['url'];
 // Generate the actual XML API uri
 $api_url = str_replace("json", "xml", $url);
 
-// Get querystring
+// Get querystring 
 $request_uri = $_SERVER['REQUEST_URI'];
 $qs = "/".str_replace($base_url, '', $request_uri);
 $qs = str_replace($url, '', $qs);
@@ -26,7 +27,7 @@ isset($_GET['return']) ? $return = "text" : $return = "json";
 if($return=="text")
 	header("Content-type: text/plain; charset=UTF-8");
 else
-	header("Content-type: application/json; charset=UTF-8");
+	header("Content-type: application/json; charset=UTF-8"); 
 
 // Callback (for jsonp)
 if(isset($_GET['callback'])) {
@@ -35,8 +36,10 @@ if(isset($_GET['callback'])) {
 } else
 	$jsonp = false;
 
+
 // Check the API url is allowed (in config.php)
 if (array_key_exists($api_url, $allowed_fields)) {
+
     
     $tourcms = new TourCMS($marketplace_account_id, $api_private_key, "simplexml");
 
@@ -44,23 +47,41 @@ if (array_key_exists($api_url, $allowed_fields)) {
 
  	$error = $result->error;
  	
+ 	
+ 	// Start building the output string
  	$string = '{
  		"tours" : [';
  	if ($error=="OK") {
  	    /* fetch associative array */
  	    foreach ($result->tour as $item) {
- 	        //printf ("%s (%s)\n", $row["name"], $row["id"]);
- 	        //print "<pre>".json_encode($row)."</pre>";
  	       	$string .= '
  			{
  			';
  			
  			foreach ($allowed_fields[$api_url] as $field) {
- 				$exploded = explode("->", $field);
- 				count($exploded) == 1 ? $value = $item->$exploded[0] : $value = $item->$exploded[0]->$exploded[1];
- 				
- 				$string .= '
- 					"'.$field.'" : '.json_encode((string)$value).',';
+ 				// Handle images separately
+ 				if($field=="images") {
+	 				$string .= '
+	 					"images" : [';
+	 					
+	 				
+	 				foreach ($item->images->image as $image) {
+	 					$string .= '
+	 						"'.$image->url . '", ';
+	 				}
+	 				
+	 				$string = substr($string, 0, strlen($string) - 2);
+	 				
+	 				$string .= '
+	 					],';
+	 			// All other fields handled simply
+ 				} else {
+ 					$exploded = explode("->", $field);
+ 					count($exploded) == 1 ? $value = $item->$exploded[0] : $value = $item->$exploded[0]->$exploded[1];
+ 					
+ 					$string .= '
+ 						"'.$field.'" : '.json_encode((string)$value).',';
+ 				}	
  			}	
  			$string = substr($string, 0, strlen($string) - 1); 
  			$string .= '
